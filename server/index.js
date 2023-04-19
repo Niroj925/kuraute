1633a50cc83763bbf0d58eae0dbb677e0d7dc7c6
Niroj Thapa <ngtc0925@gmail.com>
2023-04-19 17:27:12 +0545

    available both url

--- server/index.js
+++ server/index.js
@@ -57,8 +57,9 @@ const server=http.createServer(app);
 const io=new Server(server,{
   pingTimeout:50000,
     cors:{
-    origin:"http://localhost:3000",
+    // origin:"http://localhost:3000",
     // origin:"https://kuraute.netlify.app",
+    origin: ["https://kuraute.netlify.app, http://localhost:3000"],
     methods:["GET","POST","PUT","DELETE"]
     }
 })
