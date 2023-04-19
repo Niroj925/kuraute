920fd46485ac30d51e72eb2617a1cad858fad762
Niroj Thapa <ngtc0925@gmail.com>
2023-04-19 13:34:51 +0545

    origin url cahnge to local

--- server/index.js
+++ server/index.js
@@ -57,8 +57,8 @@ const server=http.createServer(app);
 const io=new Server(server,{
   pingTimeout:50000,
     cors:{
-    // origin:"http://localhost:3000",
-    origin:"https://kuraute.netlify.app",
+    origin:"http://localhost:3000",
+    // origin:"https://kuraute.netlify.app",
     methods:["GET","POST","PUT","DELETE"]
     }
 })
